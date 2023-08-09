172bf12b4a26e28dbf94887f41ed63d4697e5cda
San <jocksan.brittos@gmail.com>
2023-08-09 17:39:02 -0300

    Teste unitários exemplo-2 +de 1 dado

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -12,24 +12,47 @@ class SearchTest extends TestCase
      */
 
 
-    public function testGerAddressFromZipCodeDefaultUsage()
+    public function testGerAddressFromZipCodeDefaultUsage(string $input, array $esperado)
     {
         $resutado = new Search;
-        $resutado = $resutado->gerAddressFromZipCode('01001000');
-
-        $esperado = [
-            "cep" => "36500-180",
-            "logradouro" => "Rua Conselheiro Augusto César",
-            "complemento" => "",
-            "bairro" => "Copacabana",
-            "localidade" => "Ubá",
-            "uf" => "MG",
-            "ibge" => "3169901",
-            "gia" => "",
-            "ddd" => "32",
-            "siafi" => "5397"
-        ];
+        $resutado = $resutado->gerAddressFromZipCode($input);
 
         $this->assertEquals($esperado, $resutado);
+    }
+
+    public function dadosTeste()
+    {
+        return [
+            "Endereço Praça da Sé" => [
+                "01001000",
+                [
+                    "cep" => "01001-000",
+                    "logradouro" => "Praça da Sé",
+                    "complemento" => "lado ímpar",
+                    "bairro" => "Sé",
+                    "localidade" => "São Paulo",
+                    "uf" => "SP",
+                    "ibge" => "3550308",
+                    "gia" => "1004",
+                    "ddd" => "11",
+                    "siafi" => "7107"
+                ]
+            ],
+            "Endereço Qualquer" => [
+                "36500180",
+                [
+                    "cep" => "36500-180",
+                    "complemento" => "",
+                    "logradouro" => "Rua Conselheiro Augusto César",
+                    "localidade" => "Ubá",
+                    "bairro" => "Copacabana",
+                    "ibge" => "3169901",
+                    "uf" => "MG",
+                    "ddd" => "32",
+                    "gia" => "",
+                    "siafi" => "5397"
+                ]
+            ]
+        ];
     }
 }
